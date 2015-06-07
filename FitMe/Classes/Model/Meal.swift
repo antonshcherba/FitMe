0e11dc2e1f00c804134c4b407732b194842aab38
Anton Shcherba <lnk.anton@gmail.com>
2015-06-07 15:25:41 +0300

    Added class Meal that holds meals data

---
+++ FitMe/Classes/Model/Meal.swift
@@ -0,0 +1,19 @@
+//
+//  Meal.swift
+//  FitMe
+//
+//  Created by Admin on 01.06.15.
+//  Copyright (c) 2015 invalid404. All rights reserved.
+//
+
+import Foundation
+
+class Meal {
+    
+    var mealName: String = ""
+    var proteinCount: Int = 0
+    var fatsCount: Int = 0
+    var carboCount: Int = 0
+    var kilocalorieCount: Int = 0
+    
+}
