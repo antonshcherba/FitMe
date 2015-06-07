2b6cf1ae856b075a2bc4bdcfc1d288670ac8762a
Anton Shcherba <lnk.anton@gmail.com>
2015-06-07 15:22:18 +0300

    Created custom table cell for meal item

---
+++ FitMe/Classes/Views/MealCellView.swift
@@ -0,0 +1,20 @@
+//
+//  MealCellView.swift
+//  FitMe
+//
+//  Created by Admin on 01.06.15.
+//  Copyright (c) 2015 invalid404. All rights reserved.
+//
+
+import UIKit
+
+class MealCellView: UITableViewCell {
+    
+    @IBOutlet weak var mealName: UILabel!
+    @IBOutlet weak var proteinCount: UILabel!
+    @IBOutlet weak var fatsCount: UILabel!
+    @IBOutlet weak var carboCount: UILabel!
+    @IBOutlet weak var kilocalorieCount: UILabel!
+    
+    
+}
